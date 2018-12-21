7d810b0cb0bf99508ce3b27ae702c99a11764346
Adrian Obermuehlner <adrian.obermuehlner@cresoft.ch>
2018-12-21 10:02:50 +0100

    didn't selected all files in last commit, in here you find all methods to calculate

--- consoleCalculator/src/consoleCalculatorMain/Calculator.java
+++ consoleCalculator/src/consoleCalculatorMain/Calculator.java
@@ -4,6 +4,34 @@ import java.lang.*;
 public class Calculator
 {
     public int addition(int firstNumber, int secondNumber) {
-        return Integer.sum(firstNumber, secondNumber);
+        int result = Integer.sum(firstNumber, secondNumber);
+        checkSizeToLong(result);
+        return result;
+    }
+    
+    public int subtraction(int firstNumber, int secondNumber) {
+        long result = (long) firstNumber - (long) secondNumber;
+        checkSizeToShort(result);
+        return (int) result;
+    }
+    
+    public int division(int firstNumber, int secondNumber) {
+        long result = (long) firstNumber / (long) secondNumber;
+        return (int) result;
+    }
+    
+    public int multiplication(int firstNumber, int secondNumber) {
+        long result = (long) firstNumber * (long) secondNumber;
+        checkSizeToLong(result);
+        return (int) result;
+    }
+    
+    private void checkSizeToLong(long number) {
+        if ( number > Integer.MAX_VALUE)
+            throw new ArithmeticException("Result too big");
+    }
+    private void checkSizeToShort(long number) {
+        if (number < Integer.MIN_VALUE)
+            throw new ArithmeticException("Result too small");
     }
 }
--- consoleCalculator/src/consoleCalculatorMain/CalculatorMain.java
+++ consoleCalculator/src/consoleCalculatorMain/CalculatorMain.java
@@ -2,5 +2,16 @@ package consoleCalculatorMain;
 
 public class CalculatorMain
 {
-
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        
+        
+        System.out.println("Welcome to my Console Calculator");
+        System.out.println("--------------------------------------");
+        System.out.println();
+        int number1 = Integer.parseInt(args[0]);
+        int number2 = Integer.parseInt(args[1]);
+        
+        System.out.println("Summe " + number1 + " + " + number2 + " = " + calculator.subtraction(10, 20));
+    }
 }
