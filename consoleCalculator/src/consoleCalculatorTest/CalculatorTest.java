37c19020d7b51d0c190fe093f49bcaa3653758fd
Adrian Obermuehlner <adrian.obermuehlner@cresoft.ch>
2018-12-21 10:47:47 +0100

    inserted 8 tests to the function division

--- consoleCalculator/src/consoleCalculatorTest/CalculatorTest.java
+++ consoleCalculator/src/consoleCalculatorTest/CalculatorTest.java
@@ -91,13 +91,13 @@ public class CalculatorTest
     }
     
     @Test(expected=AssertionError.class)
-    public void testToBigSub1() {
+    public void testToBigSub() {
         test = new Calculator();
         assertEquals(2147483637, test.subtraction(2147483647, -50));
     }
     
     @Test(expected=AssertionError.class)
-    public void testToSmallSub1() {
+    public void testToSmallSub() {
         test = new Calculator();
         assertEquals(-2147483637, test.subtraction(-2147483647, 10));
     }
@@ -107,4 +107,56 @@ public class CalculatorTest
         test = new Calculator();
         assertEquals(0, test.subtraction(0, 0));
     }
+    
+  //division tests
+    @Test
+    public void testSimplediv1() {
+        test = new Calculator();
+        assertEquals(10, test.division(30, 3));
+    }
+    
+    @Test
+    public void testSimplediv2() {
+        test = new Calculator();
+        assertEquals(6, test.division(62, 10));
+    }
+    
+    @Test
+    public void testSimplediv3() {
+        test = new Calculator();
+        assertEquals(0, test.division(6, 10));
+    }
+    
+    @Test(expected=ArithmeticException.class)
+    public void testDividebyZero() {
+        test = new Calculator();
+        assertEquals(0, test.division(10, 0));
+    }
+    
+    @Test
+    public void testDividebyNegative() {
+        test = new Calculator();
+        assertEquals(-4, test.division(20, -5));
+    }
+    
+    @Test
+    public void testNegDividedByPos() {
+        test = new Calculator();
+        assertEquals(-4, test.division(-20, 5));
+    }
+    
+    @Test
+    public void testDivNegWithNeg() {
+        test = new Calculator();
+        assertEquals(4, test.division(-20, -5));
+    }
+    
+    @Test
+    public void testBiggestNumberDiv() {
+        test = new Calculator();
+        assertEquals(2147483647, test.division(2147483647, 1));
+    }
+    
+    
+    
 }
